502dd336200a76370dababa6e1fea87cef2370ea
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-01-22 14:24:40 +0100

    Adds Main function to stand as a placeholder. Preprocessing function to read mouse logs now does that correctly :)

---
+++ Analysis/Main.R
@@ -0,0 +1,17 @@
+#Loads the scripts in the preprocessing file
+#Most importantly - ReadMouseLog
+source("Preprocessing.R")
+path ="C:/Users/Lukáš/Vyzkum/BASIC/MouseResearch/Vystupy/UJK2"
+#mouse log takes only path value as a parameter and reads the file into a list
+ls = ReadMouseLog(path)
+#the list has three sections - you don't need to resave them, its just for demonstration purposes here
+table = ls[["table"]]
+events = ls[["events"]]
+header = ls[["header"]]
+
+View(table)
+View(events)
+View(header)
+
+#if you want to get info from the header, you can do following
+ls[["header"]]$"Reward type"
--- Analysis/PreprocessingFunctions.R
+++ Analysis/PreprocessingFunctions.R
@@ -13,22 +13,35 @@ ReadMouseLog = function(path){
   
   #reads into a text file at first
   text = readLines(path,warn=F)
-  #finds the header start
+  
+  #   reading
+  #   HEADER
   idxHeaderTop <- which(grepl('HEADER',text))
   idxHeaderBottom <- which(grepl('(\\*){5,}',text))
   headerIndexes = idxHeaderTop : idxHeaderBottom
-  #potentially returns the header as well in a list
-  ls[["header"]] <- into_list(text[(idxHeaderTop+1):(idxHeaderBottom-1)])
-  #gets the event indexes - in two steps because grepl cannot handle 
-  eventIndexes <- which(grepl('([\\-]{3,}|^[a-zA-Z]+.*$)',text[idxHeaderBottom+1:length(text)]))+idxHeaderBottom
-  non_table_indexes = c(headerIndexes,eventIndexes)
-  #fills in the voids
+  #returns the header in a list
+  ls[["header"]] <- read_header(text[(idxHeaderTop+1):(idxHeaderBottom-1)])
+  
+  #   reading
+  #   EVENTS
+  #gets the event dividers ---
+  eventDividerIndexes <- which(grepl('([\\-]{3,})',text[idxHeaderBottom+1:length(text)]))+idxHeaderBottom
+  #gets events indexes
+  eventIndexes <- which(grepl('^[a-zA-Z]+.*$',text[idxHeaderBottom+1:length(text)]))+idxHeaderBottom
+  #reads the text into the events text
+  ls[["events"]] <- read_events(text[eventIndexes])
+  
+  #   reading
+  #   TABLE
+  #combines all the lines that not to read
+  non_table_indexes = c(headerIndexes,eventDividerIndexes,eventIndexes)
   ls[["table"]]  <- read.table(textConnection(text[-non_table_indexes]),header=F,sep="")
   colnames(ls[["table"]])<-c("time","cycle","phase","lever_status","feeder_status")
+  
   return(ls)
 }
 
-into_list <- function(text =""){
+read_header <- function(text =""){
   ls <- list()
   #for each line
   for (info in text) {
@@ -39,10 +52,27 @@ into_list <- function(text =""){
     code <- split[[1]][1]
     #extracting the VALUE from the second part of the list
     value <- str_trim(str_extract_all(split[[1]][2],"[\r\n\t\f ](.*)")[[1]][1])
-    #removing the *
-    value <- substring(value,2,nchar(value)-1)
     #saving into the list 
     ls[[code]] <- value
   }
   return(ls)
+}
+
+read_events <-function(text=""){
+  #preallocating the table
+  frame <- data.frame(event = character(length(text)),time = numeric(length(text)),stringsAsFactors = F)
+  i = 1
+  #for each line
+  for (info in text) {
+    #finds the EVENT NAME
+    split <- str_split(info, pattern = ";",n=2)
+    #extract the value from the str_split list (this is a weird line but
+    #strsplit creates a list of lists so we need to do this
+    frame$event[i] <- str_trim(split[[1]][1])
+    #extracting the VALUE from the second part of the list
+    frame$time[i] <- as.numeric(str_trim(split[[1]][2]))
+    #saving into the list 
+    i=i+1
+  }
+  return(frame)
 }
