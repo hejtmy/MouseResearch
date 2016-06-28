83310e94f5c00011012a10b00074ccb640e1f428
hejtmy <hejtmy@gmail.com>
2016-06-28 19:54:00 +0200

    Small typs and movment of part

--- Analysis/Preprocessing/HelperPreprocessing.R
+++ Analysis/Preprocessing/HelperPreprocessing.R
@@ -1,14 +1,3 @@
-CreateAnalysesTable = function(analysisList){
-  analysesTable = data.table()
-  for(analysis in timeAnalyses){
-    print(analysis$name)
-    analysis$MakeBetterEventTable()
-    analysis$betterEventTable$name = rep(analysis$name, nrow(analysis$betterEventTable))
-    analysesTable = rbind(analysesTable, analysis$betterEventTable)
-  }
-  write.table(analysesTable, "analysesTable.csv", sep=";", row.names = F, quote = F)
-}
-
 CreateAnalysesList = function(path){
   files = list.files(path, full.names = T, include.dirs = F)
   timeAnalyses = list()
@@ -18,4 +7,15 @@ CreateAnalysesList = function(path){
     timeAnalyses[[analysis$name]] = analysis
   }
   save(timeAnalyses, file = "timeAnalyses.RData")
+}
+
+CreateAnalysesTable = function(analysisList){
+  analysesTable = data.table()
+  for(analysis in timeAnalyses){
+    print(analysis$name)
+    analysis$MakeBetterEventTable()
+    analysis$betterEventTable$name = rep(analysis$name, nrow(analysis$betterEventTable))
+    analysesTable = rbind(analysesTable, analysis$betterEventTable)
+  }
+  write.table(analysesTable, "analysesTable.csv", sep=";", row.names = F, quote = F)
 }
