b4df771698d9474596fe7f0e8b02d175258ac624
hejtmy <hejtmy@gmail.com>
2016-07-21 14:11:23 +0200

    renames variables

--- Analysis/Main.R
+++ Analysis/Main.R
@@ -7,9 +7,9 @@ source("AnalysisFunctions.R")
 #check if there is a file with the data
 load("timeAnalyses.RData")
 #if not, read in the data
-betterEventTable = fread("analysesTable.csv", sep=";", header=T)
+event_table = fread("analysesTable.csv", sep=";", header=T)
 # check if you can read the saved table
-ls = GetLeverPressTimes(betterEventTable)
+ls = lever_press_times(event_table)
 pressTable = ls$pressTable
 releaseTable = ls$releaseTable
 ls = NULL
@@ -17,5 +17,6 @@ ls = NULL
 singleTable = pressTable %>% filter(name == '10_C4TCO.G')
 new = singleTable %>% group_by(phaseIndex) %>% summarise(mean=mean(timeSincePhaseStart))
 ggplot(new, aes(phaseIndex, mean)) + geom_path()
-new = singleTable %>% group_by(phaseIndex) %>% summarise(max=max())
+
+new = singleTable %>% group_by(phaseIndex)
 ggplot(new, aes(order, leverPresses)) + geom_count(stat = "identity")
