3eb7a61a00453488fca48225c69cc48f6fd64f5c
hejtmy <hejtmy@gmail.com>
2016-07-22 19:45:10 +0200

    cleans up styling

--- Analysis/Preprocessing/create_header_table.R
+++ Analysis/Preprocessing/create_header_table.R
@@ -1,12 +1,19 @@
-library(data.table)
 #takes list of TimeAnalysis functions and saves table with header information for each patient
-create_header_table = function(analysisList){
+create_header_table = function(ls_analysis){
   ls = list()
-  for(analysis in analysisList){
-    patientRow = list(name = analysis$name, session = analysis$session, experimentType = analysis$header$`Experiment type`, numberOfCycles = analysis$header$`Number of cycles`, testCycleDuration = analysis$header$`Duration of each test cycle`, rewardType= analysis$header$`Reward type`, rewardNumber= analysis$header$`Reward number`, rewardDuration= analysis$header$`Duration of reward cycle`, interTrialTime = analysis$header$`Inter trial time`)
-    ls[[analysis$name]] = patientRow
+  for(analysis in ls_analysis){
+    row = list(name = analysis$name, 
+                      session = analysis$session, 
+                      experimentType = analysis$header$`Experiment type`, 
+                      numberOfCycles = analysis$header$`Number of cycles`, 
+                      testCycleDuration = analysis$header$`Duration of each test cycle`, 
+                      rewardType = analysis$header$`Reward type`, 
+                      rewardNumber = analysis$header$`Reward number`, 
+                      rewardDuration = analysis$header$`Duration of reward cycle`, 
+                      interTrialTime = analysis$header$`Inter trial time`)
+    ls[[analysis$name]] = row
   }
-  headerTable = rbindlist(ls)
-  write.table(headerTable, "HeaderTable.csv", sep=";", row.names = F, quote = F)
-  return(headerTable)
+  header_table = rbindlist(ls)
+  write.table(header_table, "header_table.csv", sep=";", row.names = F, quote = F)
+  return(header_table)
 }
