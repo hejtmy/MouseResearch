3f56cc2382455a1c3c0225e492e4de0efe0a6e58
hejtmy <hejtmy@gmail.com>
2016-07-25 10:46:19 +0200

    Renamed things in the merged table for clarity

--- Analysis/Preprocessing/lever_press_times.R
+++ Analysis/Preprocessing/lever_press_times.R
@@ -1,4 +1,5 @@
 #Gets the time since start of lever preses in a table for given phase
+
 lever_press_times = function(event_table){
   experimentPhases = c("Test", "Reward", "InterTrial")
   phasesTimes = data.table(event_table[type %in% experimentPhases,])
@@ -7,14 +8,14 @@ lever_press_times = function(event_table){
   
   leverTimes = data.table(event_table[type == "Lever",])
   #merges all possibl elevers with all phases for each participatn and then selects only mathing
-  newTable = merge(leverTimes, phasesTimes, by="name", allow.cartesian = T)
+  newTable = merge(leverTimes, phasesTimes, by = "name", allow.cartesian = T, suffixes = c("_event", "_phase"))
   ls = list()
-  ls$pressTable = unique(newTable[startTime.x > startTime.y & startTime.x < endTime.y], by = NULL)
-  ls$pressTable[, time_since_phase_start := startTime.x - startTime.y[1], by=list(name, type.y, phaseIndex)]
-  ls$pressTable[, time_since_cycle_start := startTime.x - cycleStart]
-  ls$pressTable[, lever_phase_id := c(1:.N), by=list(name, type.y, phaseIndex)]
+  ls$pressTable = unique(newTable[startTime_event > startTime_phase & startTime_event < endTime_phase], by = NULL)
+  ls$pressTable[, time_since_phase_start := startTime_event - startTime_phase[1], by=list(name, type_phase, phaseIndex)]
+  ls$pressTable[, time_since_cycle_start := startTime_event - cycleStart]
+  ls$pressTable[, lever_phase_id := c(1:.N), by=list(name, type_phase, phaseIndex)]
   
-  ls$releaseTable = unique(newTable[endTime.x > startTime.y & endTime.x < endTime.y], by = NULL)
+  ls$releaseTable = unique(newTable[endTime_event > startTime_phase & endTime_event < endTime_phase], by = NULL)
   
   return(ls)
 }
